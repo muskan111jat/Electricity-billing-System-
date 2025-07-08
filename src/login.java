67896f4802a5325262258e2b6284bb4d80683176
muskan111jat <muskanjat803@gmail.com>
2025-07-08 17:49:37 +0530

    update

---
+++ src/login.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+
+class Login extends JFrame {
+    Login() {
+        setSize(500, 500);
+        setVisible(true);
+        try {
+            Thread.sleep(2000);//frame will  hold for 2 sec then it will automatically close
+            setVisible(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
+}
+
+
+
+
